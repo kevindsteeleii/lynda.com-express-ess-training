545d7b21c202ae4d93524a1740e1004595c90dee
kevindsteeleii <anubis523@gmail.com>
2020-12-14 22:44:22 -0500

    finished

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 import express from "express";
+import favicon from "serve-favicon";
+import path from "path";
 import data from "./data/data.json";
+import helmet from "helmet";
 
 const app = express();
 const PORT = 3000;
 
+app.use(helmet());
 app.use(express.static('public'));
 app.use(express.static('images'));
+
+// app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// this is for proxies
+app.set('trust proxy', 'loopback');
+
+app.use('/images', express.static('images'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico' )));
 
 app.get('/', (req, res) => {
   // res.send(data);
   // get data
-  res.json(data);
+  res.setHeader('Content-Type', 'application/json');
+  // res.json(data);
+  res.send(JSON.stringify(data, null, 2));
 });
 
-// app.get('/images', (req, res, next) => {
-//   // res.redirect("http://www.linkedin.com");
-//   res.download("images/rocket.jpg");
-// });
+app.post('/newItem', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.get('/item/:id', (req, res, next) => {
-  // res.send(data);
-  // get data
+  // this is the middleware that pulls the data
   let userNo = +req.params.id;
-  if(data.length < userNo) {
-    res.send(`<h1>Requested User with ID: ${userNo} does not exist.</h1>`)
-  } else {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(404).send(JSON.stringify(data[userNo], null, 2));
-    // res.send(JSON.stringify(data[userNo], null,));
-  }
+  let user = data[userNo];
+  // everything above is Middleware
+  console.log(`Request from: ${req.originalUrl}`);
+  console.log(`Request type: ${req.method}`);
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(user, null, 2));
+  // if(data.length < userNo) {
+  //   res.send(`<h1>Requested User with ID: ${userNo} does not exist.</h1>`)
+  // } else {
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.status(200).send(JSON.stringify(data[userNo], null, 2));
+  // }
   next();
 }, (req, res) => {
   console.log('Did you get the right data?')
@@ -37,7 +56,8 @@ app.get('/item/:id', (req, res, next) => {
 
 app.route('/item')
   .get((req, res) => { 
-    res.send(`a get request with /item route on port ${PORT}`) 
+    throw new Error('Not working');
+    // res.send(`a get request with /item route on port ${PORT}`) 
     })
   .post((req, res) => { 
     res.send(`a post request with /item route on port ${PORT}`) 
@@ -53,6 +73,11 @@ app.get("*", (req, res) => {
   res.redirect("/");
 });
 
+// error handling function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(404).send(`Red alert! Red Alert!: \n${err.stack}`)
+});
 
 app.listen(PORT, () => {
   console.log(`Serving running on port ${PORT}`);
